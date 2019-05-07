110686d98c1a430bbaf2ed3b3cb587bb3e76914c
Roberto A. Rodriguez <50374184+roberto-alexis-rodriguez@users.noreply.github.com>
2019-05-07 18:25:40 -0500

    Update movie.sh

--- movies/movie.sh
+++ movies/movie.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+  
+vmd -dispdev text -e movie.tcl
 
 # you may change the Constant Rate Factor (crf)
 # to be larger than 18 to get smaller filesizes in exchange
@@ -6,4 +8,5 @@
 # that tends to produce enormous movies if the underlying images
 # are already large
 
-ffmpeg -framerate 24 -i images/%03d.tga -c:v libx264 -profile:v high -crf 18 -pix_fmt yuv420p -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" movie.avi
+output=opening.avi
+ffmpeg -framerate 24 -i images/%03d.tga -c:v libx264 -profile:v high -crf 18 -pix_fmt yuv420p -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" $output
