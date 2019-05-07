64c2f377ca4105e0b331094f8ff2bb51afa0d615
roberto-alexis-rodriguez <50374184+roberto-alexis-rodriguez@users.noreply.github.com>
2019-05-07 10:49:20 -0500

    Create render.tcl

---
+++ movies/render.tcl
@@ -0,0 +1,16 @@
+set i 0
+for {set fr 4800} {$fr < 5000} {incr fr} {
+    animate goto $fr
+    if {$i<10} {
+        set fn 00${i}
+    } elseif {$i>=10 && $i<100} {
+        set fn 0${i}
+    } else {
+        set fn $i
+    }
+    render Tachyon images/$fn "/usr/local/lib/vmd/tachyon_LINUXAMD64 -aasamples 12 %s -o %s.tga"
+    file delete images/$fn
+    incr i
+}
+
+exit
